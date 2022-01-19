4a688f02f4c2adeb852e91180cb78f3315cb9274
philip neighbour <philip.neighbour@welkinsciences.com>
2022-01-19 14:55:57 -0700

    polynomial class added

--- examplecode/functions.py
+++ examplecode/functions.py
@@ -15,3 +15,37 @@ def x_squared(x):
 
 def x_cubed(x):
     return x*x*x
+
+class Polynomial:
+    """
+    Stores data for a polynomial
+    Examples:
+        >>> poly = Polynomial([1,2,3]) # 1+2*t+t**2
+        >>> print(poly)
+        >>> print(poly.eval(4)) # 57=4**2*3+4**1*2+4**0*1
+    """
+    def __init__(self,coeficiants):
+        self._coef = coeficiants
+    def eval(self,t=0):
+        """ evaluates the polinomial at some value """
+        return sum([coef * t**n for n,coef in enumerate(self._coef)])
+    def __repr__(self):
+        ret=''
+        for n,coef in enumerate(self._coef):
+            ret+=f'{"%+g"%coef}*t**{n}'
+        return ret
+
+class Quadratic(Polynomial):
+    """
+    A quadratic is a polynomial of degree 2
+    Examples:
+        >>> poly = Quadratic([6,5,1])
+        >>> print(poly.roots()) # (x+2)*(x+3) has roots at -2,-3
+    """
+    def __init__(self,coeficiants):
+        assert len(coeficiants) <= 3
+        Polynomial.__init__(self,coeficiants)
+    def roots(self):
+        """ returns the roots of the polynomial using the quadratic formula """
+        c,b,a=self._coef
+        return [-b+pm*(b**2-4*a*c)**(1/2)/(2*a) for pm in [-1,1]]
